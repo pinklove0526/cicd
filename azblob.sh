750722c7156b265b985bd7fbca82b488d86b295e
Jellyfish Ng <72180039+pinklove0526@users.noreply.github.com>
2024-01-27 10:39:16 +0700

    Update azblob.sh

--- azblob.sh
+++ azblob.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-az storage blob upload --container-name '$web' --file ./index.html --account-name ndtan1601@hotmail.com --account-key $key1
+az storage blob upload --container-name '$web' --file ./index.html --account-name sccounttest --account-key $key1
